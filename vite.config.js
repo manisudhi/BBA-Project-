ed6d25f79f74c8e58a3b0aed455ee668f93718eb
manisudhi <maniksudhi@gmail.com>
2026-08-11 21:46:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/BBA-Project-/',
   plugins: [react()],
   build: {
     emptyOutDir: true,
